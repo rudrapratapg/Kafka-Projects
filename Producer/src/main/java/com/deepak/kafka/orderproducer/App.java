095f070d4725538a722e867ec7dfe95c87e400b0
Deepak Gola <er.deepakgola@gmail.com>
2025-03-15 18:48:08 +0530

    Initial Project commit Added kafka-client and jackson-databind to the project pom files

---
+++ Producer/src/main/java/com/deepak/kafka/orderproducer/App.java
@@ -0,0 +1,13 @@
+package com.deepak.kafka.orderproducer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
