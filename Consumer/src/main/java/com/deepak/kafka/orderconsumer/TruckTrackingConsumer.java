dee090e1ac9c8449b386cbfa262a85bfbc86d608
Deepak Gola <er.deepakgola@gmail.com>
2025-03-16 23:02:22 +0530

    Created and implemented custom deserializer of TruckTracking type

--- Consumer/src/main/java/com/deepak/kafka/orderconsumer/TruckTrackingConsumer.java
+++ Consumer/src/main/java/com/deepak/kafka/orderconsumer/TruckTrackingConsumer.java
@@ -7,23 +7,25 @@ import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import com.deepak.kafka.orderconsumer.customdeserializers.TruckTracking;
+
 public class TruckTrackingConsumer {
 
 	public static void main(String[] args) {
 		Properties props = new Properties();
 		props.setProperty("bootstrap.servers","localhost:9092");
 		props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
-		props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.setProperty("value.deserializer", "com.deepak.kafka.orderconsumer.customdeserializers.TruckTrackingDeserializer");
 		props.setProperty("group.id", "TruckTrackingGroup");
 		
-		KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
+		KafkaConsumer<Integer, TruckTracking> consumer = new KafkaConsumer<>(props);
 		
 		String topicName = "TruckTracking";
 		try {
 			consumer.subscribe(Collections.singletonList(topicName));
-			ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofSeconds(20));
+			ConsumerRecords<Integer, TruckTracking> records = consumer.poll(Duration.ofSeconds(20));
 			records.forEach( record -> {
-				System.out.println("Truck ID:: "+record.key()+" Coordinates:: "+record.value());
+				System.out.println("Truck ID:: "+record.key()+" Coordinates:: "+record.value().toString());
 			});
 		} catch (Exception e) {
 			e.printStackTrace();
