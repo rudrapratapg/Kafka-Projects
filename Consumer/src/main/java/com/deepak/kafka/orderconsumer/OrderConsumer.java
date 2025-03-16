9061adb0efd7cca95aa1ee8b1552563ac283b795
Deepak Gola <er.deepakgola@gmail.com>
2025-03-16 22:08:40 +0530

    Added Custom Deserializer of Order type

--- Consumer/src/main/java/com/deepak/kafka/orderconsumer/OrderConsumer.java
+++ Consumer/src/main/java/com/deepak/kafka/orderconsumer/OrderConsumer.java
@@ -8,19 +8,21 @@ import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import com.deepak.kafka.orderconsumer.customdeserializers.Order;
+
 public class OrderConsumer {
 	
 	public static void main(String args[]) {
 		Properties props = new Properties();
 		props.setProperty("bootstrap.servers","localhost:9092");
 		props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+		props.setProperty("value.deserializer", "com.deepak.kafka.orderconsumer.customdeserializers.OrderDeserializer");
 		props.setProperty("group.id", "OrderGroup");
 		
-		KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);
+		KafkaConsumer<String, Order> consumer = new KafkaConsumer<>(props);
 		consumer.subscribe(Collections.singletonList("OrderTopic"));
 		
-		ConsumerRecords<String, Integer> orders = consumer.poll(Duration.ofSeconds(20));
+		ConsumerRecords<String, Order> orders = consumer.poll(Duration.ofSeconds(20));
 		orders.forEach( order -> {
 			System.out.println("Product name:: "+order.key()+" Quantity:: "+order.value());
 		});
